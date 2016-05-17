068bb5d934b667fc2ea289cce5414a6eda3f2fc7
jbn566 <jbn566@gmail.com>
2016-05-17 14:47:33 -0400

    Inital RNG functions and Randy static value using Threadsafe

---
+++ CodeLibraries/common.rng/RNG.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB2.Common
+{
+    public class RNG
+    {
+
+        private static int _randy = 0;
+
+        public static int ThreadSafe(int min, int max)
+        {
+            return ThreadSafeRandom.ThisThreadsRandom.Next(min, max);
+        }
+        public static int ThreadSafe(int max)
+        {
+            return ThreadSafeRandom.ThisThreadsRandom.Next(max);
+        }
+
+
+        public static int Randy
+        {
+            get
+            {
+                int newRandy = ThreadSafe(1, 65535);
+
+                _randy = newRandy;
+
+                return _randy;
+
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Solutions/RandomRandy/console.test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(JB2.Common.RNG.Randy);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
